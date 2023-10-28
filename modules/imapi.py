ac74c8ce1cd2aee2690728dda8074eb7ce37c5e1
Raven Ferriols <142744952+RavenMF@users.noreply.github.com>
2023-10-28 19:58:51 +0800

    Update imapi.py

--- modules/imapi.py
+++ modules/imapi.py
@@ -3,7 +3,13 @@ import random
 import re
 
 def linklist(search):
-  a = rq.get(f"https://unsplash.com/s/photos/{search}").text
+  try:
+    b = rq.get(f"https://unsplash.com/s/photos/{search}")
+  except:
+    return "Error occured"
+    
+  if b.status_code == 200:
+    a = b.text
   regex = r"https://images\S+.=80"
   c = re.findall(regex, a)
   return c
@@ -12,3 +18,13 @@ def single(search):
   list = linklist(search)
   return list[random.randint(0,len(list))]
   
+def download(search, ex):
+  if ex == None:
+    ex = "png"
+  url = single(search)
+  try:
+    img = rq.get(url).content
+    open(f"{search}.{ex}", "wb").write(img)
+    return search+"."+ex
+  except:
+    return "Error occured"
